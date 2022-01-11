e34115c6a890daf4e19025289787b685f946e104
slasken06 <56585404+slasken06@users.noreply.github.com>
2022-01-11 21:04:14 +0100

    Added game jam generator

---
+++ JamGenerator/parts.js
@@ -0,0 +1,45 @@
+var parts = {
+    "Themes": [
+        "Drop",
+        "Joined together",
+        "Physics",
+        "Chaos",
+        "Climb",
+        "Dual purpose",
+        "Fish & fire"
+    ],
+    "WildCards": [
+        {
+            "name": "Infiniplay",
+            "description": "Make the game be infinitly playable."
+        },
+        {
+            "name": "Real data", 
+            "description": "Use real life data in some form."
+        },
+        {
+            "name": "Dimentional swap",
+            "description": "Be able to switch between 2D and 3D at any time."
+        },
+        {
+            "name": "Limited space",
+            "description": "Make the entire game fit within a single 64x64 pixels window."
+        },
+        {
+            "name": "The good old days",
+            "description": "Make the game in grayscale."
+        },
+        {
+            "name": "SSStylish",
+            "description": "Include a style meter of some sorts."
+        },
+        {
+            "name": "On the clock",
+            "description": "Include a time limit."
+        },
+        {
+            "name": "Combos",
+            "description": "Add a combo system of some sorts."
+        }
+    ]
+}
---
+++ JamGenerator/script.js
@@ -0,0 +1,21 @@
+function randomselect(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+window.onload = function() {
+    // Select theme
+    console.log(parts.Themes[0]);
+    var x = document.getElementById("theme");
+    x.innerHTML = randomselect(parts.Themes);
+
+    // Select wildcard
+    // Loop through 3 numbers
+    for (var i = 0; i < 3; i++) {
+        var wildcard = randomselect(parts.WildCards);
+        var wildcardname = document.getElementById("wildcard" + i + "_name");
+        var wildcarddescription = document.getElementById("wildcard" + i + "_description");
+
+        wildcardname.innerHTML = wildcard.name;
+        wildcarddescription.innerHTML = wildcard.description;
+    }
+}
